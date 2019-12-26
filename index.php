29a97944e588bcd4d8c358df30fffbc944a72640
Павел Палий <paliipavel@gmail.com>
2019-12-26 15:02:37 +0200

    3

--- index.php
+++ index.php
@@ -6,20 +6,24 @@ function get_tree()
 {
 	$pdo = new \PDO("mysql:host=" . HOST . "; dbname=" . DBNAME . ";charset=utf8;", USER, PASS, [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION]);
 	
-	 $query = "SELECT id, title, parent_id
+	 $query = "SELECT categories_id, parents_id
 	 FROM categories";
 	 $cat = $pdo->query($query);
 	 $categories = $cat->fetchAll(PDO::FETCH_ASSOC);
-	 
+	 $map = array(
+	         0 => array('subcategories' => array())
+	     );
 	 foreach ($categories as &$category) {
+	        //в массиве создается подмассив с полем 'subcategories'
 	         $category['subcategories'] = array();
-	         $map[$category['id']] = &$category;
+	         /*в массив $map заносим категорию, передача идет по ссылке, потому что иначе категории не будут обновляться при добавлении подкатегорий*/
+	         $map[$category['categories_id']] = &$category;
+	         /* добавление категорий в массив 'subcategories' родителя */
+	         $map[$category['parents_id']]['subcategories'][] = &$category;
+	         
 	     }
 
-	     foreach ($categories as &$category) {
-	         $map[$category['parent_id']]['subcategories'][] = &$category;
-	     }
-
+	     
 	     return $map[0]['subcategories'];
 	
 }
